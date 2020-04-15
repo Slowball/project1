37c22d2943427644d97d5b29a14d64bf9f49be97
Serhii <monteki93@gmail.com>
2020-04-15 19:16:16 +0300

    plus cikl FOR

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -17,5 +17,18 @@ let a1 = prompt("Введите обязательную статью расхо
 
 appData.expenses[a1] = a2;
 appData.expenses[a3] = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = +prompt("Во сколько обойдется?", "");
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+    && a != '' && b != '' && a.length < 50) {
+    appData.expenses[a] = b; 
+} else {
+    
+    i--;
+    }
+}
+appData.moneyPerDay = appData.budget / 30;
+alert(`Budget per day: ${appData.moneyPerDay}`);
 
-alert(appData.budget / 30);
+
