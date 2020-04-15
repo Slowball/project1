85d3f5330eaa52071438d8b37d07aa012694cece
Serhii <monteki93@gmail.com>
2020-04-15 18:46:42 +0300

    Restruct javascript

--- java.js
+++ java.js
@@ -1,12 +1,21 @@
-let money = prompt("Vash budjet?", "");
-let time = prompt("Введите дату в формате ЮЮЮЮ-ММ-ДД", "");
-let appData = {};
-appData.timeData = time;
-appData.money =  money;
-let howMeny = prompt("Vvedite obiazatelnu statu rashodov", "");
-let howMuch = prompt("Vo skolko oboidetsa", "");
-appData.expenses = {howMeny: howMuch};
-appData.optionalExpenses = {};
-appData.income = [];
-appData.savings = false;
-alert(money / 30);
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budget / 30);
