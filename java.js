570f009c76f6e1eff90106c2491ba4d297b96db1
Serhii <monteki93@gmail.com>
2020-04-15 19:21:29 +0300

    GET while

--- java.js
+++ java.js
@@ -10,23 +10,17 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
-for (let i = 0; i < 2; i++) {
+
+let i = 0
+while (i < 2) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", "");
     let b = +prompt("Во сколько обойдется?", "");
     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
     && a != '' && b != '' && a.length < 50) {
     appData.expenses[a] = b; 
-} else {
-    
-    i--;
-    }
+} 
+    i++;
 }
 appData.moneyPerDay = appData.budget / 30;
 alert(`Budget per day: ${appData.moneyPerDay}`);
