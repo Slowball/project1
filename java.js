6f0bd8bdc4629ba9f290b0f6fd2f142bfb0803e9
Serhii <monteki93@gmail.com>
2020-04-15 22:28:15 +0300

    add function

--- java.js
+++ java.js
@@ -1,28 +1,37 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+}
+start();
 
 let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
 
 
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+        let b = +prompt("Во сколько обойдется?", "");
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+            a != '' && b != '' && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        };
+    };
 
-let i = 0
-while (i < 2) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
-    let b = +prompt("Во сколько обойдется?", "");
-    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
-    && a != '' && b != '' && a.length < 50) {
-    appData.expenses[a] = b; 
-} 
-    i++;
-}
-appData.moneyPerDay = appData.budget / 30;
+};
+chooseExpenses();
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 alert(`Budget per day: ${appData.moneyPerDay}`);
-
-
